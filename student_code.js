54010a8aec1dadc2cb8c729d23481b76960ba60f
IsabelWickliffe <Isabel.Wickliffe@gmail.com>
2016-11-10 14:40:19 -0500

    Add files via upload

---
+++ student_code.js
@@ -0,0 +1,101 @@
+// Isabel Wickliffe: I worked on this assignment alone using only this semester's course materials
+
+kiwi_caught = 0;
+
+function process_frame() {
+  if (is_down("b")) {
+    brake_truck();
+  } else if (is_down("left")) {
+    translate_truck_left();
+  } else if (is_down("right")) {
+    translate_truck_right();
+  } else {
+    coast_truck();
+  }
+  compute_truck_position();
+  if (Math.round(100*Math.random()) < kiwi_rate) {
+    create_kiwi();
+    console.log(get_kiwi_x());
+
+  }
+  // for each frame we want to use the random function and the user probability to decide whether to drop a kiwi
+}
+
+function translate_truck_right() {
+  // add to the current truck velocity unless its greater than or equal to a maximum value
+  velocity = get_truck_velocity();
+  velocity = velocity + 0.4;
+  if (velocity > 20) {
+    velocity = 20;
+  }
+  set_truck_velocity(velocity);
+	// This function should increase the truck's velocity by some constant. There should be a maximum velocity to keep the truck from accelerating infinitely. These exact values are up to you.
+}
+
+function translate_truck_left() {
+  // add to the current truck velocity unless its greater than or equal to a maximum value
+  velocity = get_truck_velocity();
+  velocity = velocity - 0.4;
+  if (velocity < -20) {
+    velocity = -20;
+  }
+  set_truck_velocity(velocity);
+}
+
+function coast_truck() {
+	velocity = get_truck_velocity();
+  // if velocity is positive do something
+  // otherwise do something
+  if (velocity > 0.4) {
+    velocity = velocity - 0.1;
+  } else if (velocity < -0.4) {
+    velocity = velocity + 0.1;
+  } else {
+    velocity = 0;
+  }
+  set_truck_velocity(velocity);
+}
+
+function brake_truck() {
+  velocity = get_truck_velocity();
+  // if velocity is positive do something
+  // otherwise do something
+  if (velocity > 5) {
+    velocity = velocity - 5;
+  } else if (velocity < -5) {
+    velocity = velocity + 5;
+  } else {
+    velocity = 0;
+  }
+  set_truck_velocity(velocity);
+}
+
+function compute_truck_position() {
+  leftDis = get_truck_left();
+  rightDis = window.innerWidth - leftDis;
+  velocity = get_truck_velocity();
+  // current position plus velocity
+  newPos = leftDis + velocity;  // time is already defined by frame buffer
+  set_truck_left(newPos);
+  // add boundaries
+}
+
+function find_collisions(kiwi) {
+  // get the coordinates for truck bed
+  // get the coordinates the kiwis???
+      // do i do all kiwis (array)
+  // see if the kiwi coordinates are within the truck coordinates (bounds???)
+      // what about entering from the side?
+  // if they are the same, add 1 to kiwi count
+
+
+  // upon kiwi spawn, begin find collisions for that kiwi (x, y blah blah)
+  get_kiwi_x(kiwi);
+}
+
+function game_over() {
+	// your code here
+}
+
+// just ignore this (‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌but don't delete it‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌):
+check_latest = 2;
